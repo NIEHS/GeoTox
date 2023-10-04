7e2c3f999c1d19ec7567a8622a4aa312face4647
SkylarMarvel <skylar.marvel@gmail.com>
2023-10-04 18:34:10 -0400

    Added 2-param hill functions, other minor changes

--- R/tcplHillVal.R
+++ R/tcplHillVal.R
@@ -3,15 +3,15 @@
 #' @description
 #' Calculate the efficacy for a given concentration in regular space
 #'
-#' @param c concentration in regular space
+#' @param conc concentration in regular space
 #' @param tp top asymptote
 #' @param ga AC50
 #' @param gw hill coefficient
 #' @param bt bottom asymptote
 #'
 #' @return response value
-tcplHillVal <- function(c, tp, ga, gw, bt = 0) {
+tcplHillVal <- function(conc, tp, ga, gw, bt = 0) {
 
-  bt + (tp - bt) / (1 + (ga / c)^gw)
+  bt + (tp - bt) / (1 + (ga / conc)^gw)
 
 }
