38b303eb735b74dcf027dc643b58aae2f372ef92
SkylarMarvel <skylar.marvel@gmail.com>
2023-11-03 17:41:12 -0400

    Moved dplyr out of extract_hill_params function

--- R/extract_hill_params.R
+++ R/extract_hill_params.R
@@ -6,9 +6,14 @@
 #' @export
 #'
 #' @examples
+#' df <- data.frame(
+#'   logc = -3:3,
+#'   resp = 5 / (1 + 10^(1.2 * (0.4 - rep(-3:3, each = 3)))) + rnorm(21)
+#'  )
 #' fit <- fit_hill(df$logc, df$resp)
 #' extract_hill_params(list(fit))
 #'
+#' df_list <- list(df, df)
 #' fits <- lapply(df_list, function(df) {fit_hill(df$logc, df$resp)})
 #' extract_hill_params(fits)
 extract_hill_params <- function(fits) {
@@ -16,30 +21,25 @@ extract_hill_params <- function(fits) {
   # TODO add ability to extract from 3-param fits?
 
   # Convert list into data frame
-  df_params <- do.call(
-    rbind,
-    lapply(fits, function(fit) {
-      tibble::as_tibble(t(unlist(fit))) %>%
-        dplyr::rename(
-          tp         = par.tp,
-          tp.sd      = sds.tp,
-          logAC50    = par.logAC50,
-          logAC50.sd = sds.logAC50
-        ) %>%
-        dplyr::select(
-          tp, tp.sd, logAC50, logAC50.sd,
-          logc_min, logc_max, resp_min, resp_max, AIC
-        ) %>%
-        dplyr::mutate(dplyr::across(tp:AIC, ~ as.numeric(.x)))
-    })
-  )
+  df_params <- do.call(rbind, lapply(fits, function(fit) {
+    fit_params <- as.data.frame(t(unlist(fit)))
+    fit_params <- fit_params[c(
+      "par.tp", "sds.tp", "par.logAC50", "sds.logAC50",
+      "logc_min", "logc_max", "resp_min", "resp_max", "AIC"
+    )]
+    names(fit_params)[c(1:4)] <- c("tp", "tp.sd", "logAC50", "logAC50.sd")
+    fit_params
+  }))
 
   # Impute sd NAs with mean
-  df_params %>%
-    dplyr::mutate(
-      tp.sd.imputed      = is.na(tp.sd),
-      logAC50.sd.imputed = is.na(logAC50.sd),
-      tp.sd      = dplyr::if_else(is.na(tp.sd), tp, tp.sd),
-      logAC50.sd = dplyr::if_else(is.na(logAC50.sd), logAC50, logAC50.sd)
-    )
+  df_params$tp.sd.imputed <- is.na(df_params$tp.sd)
+  idx <- df_params$tp.sd.imputed
+  df_params$tp.sd[idx] <- df_params$tp[idx]
+
+  df_params$logAC50.sd.imputed <- is.na(df_params$logAC50.sd)
+  idx <- df_params$logAC50.sd.imputed
+  df_params$logAC50.sd[idx] <- df_params$logAC50[idx]
+
+  # Return
+  df_params
 }
