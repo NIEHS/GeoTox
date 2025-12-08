7f1e4fbc55a5ec7fcabe62dde8ec3b05c69469ca
SkylarMarvel <skylar.marvel@gmail.com>
2025-12-08 10:43:40 -0700

    Fixed test due to C_ss bug change

--- tests/testthat/test-get_fixed_css.R
+++ tests/testthat/test-get_fixed_css.R
@@ -3,11 +3,13 @@ test_that("errors", {
 })
 
 test_that("results", {
-  simulated_css <- list(tibble::tibble(age_min = 1,
-                                       age_median_css = 5,
-                                       weight = "Normal",
-                                       weight_median_css = 10,
-                                       css = list(c(15, 15))))
+  simulated_css <- list(
+    "cas1" = tibble::tibble(age_min = 1,
+                            age_median_css = 5,
+                            weight = "Normal",
+                            weight_median_css = 10,
+                            css = list(c(15, 15)))
+  )
   C_ss <- list(matrix(7))
   age <- 2
   obesity <- "Normal"
@@ -16,10 +18,15 @@ test_that("results", {
                        C_ss = C_ss,
                        age = age,
                        obesity = obesity)
-  
-  expect_equal(out,
-               list(age = list(matrix(5)),
-                    params = list(matrix(15)),
-                    obesity = list(matrix(10)),
-                    other = list(7)))
+
+  expected <- list(
+    age = list(matrix(5)),
+    params = list(matrix(15)),
+    obesity = list(matrix(10)),
+    other = list(7)
+  )
+  dimnames(expected$age[[1]]) <- list(NULL, "cas1")
+  dimnames(expected$params[[1]]) <- list(NULL, "cas1")
+  dimnames(expected$obesity[[1]]) <- list(NULL, "cas1")
+  expect_equal(out, expected)
 })
